39c6bac9e6ad72a7089daa9745ffaa8864c791ae
urbeingwatched8 <35970083+urbeingwatched8@users.noreply.github.com>
2018-11-01 23:46:41 +0300

    Update ex1.c

--- week11/ex1.c
+++ week11/ex1.c
@@ -1,11 +1,12 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/mman.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 int main(int argc, const char *argv[])
 {
